f112af4b0ddc1f7e823959190edfd7a2b7b770dc
Olzumzum <evdokimova_olga_15@mail.ru>
2020-08-17 15:24:27 +0700

    databinding from include fragment_book_by_date

--- app/src/main/java/com/olzumzum/booklib/ui/listbydata/BookByDateFragment.kt
+++ app/src/main/java/com/olzumzum/booklib/ui/listbydata/BookByDateFragment.kt
@@ -9,8 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.olzumzum.booklib.R
 import com.olzumzum.booklib.app.App
+import com.olzumzum.booklib.databinding.FragmentBookByDateBinding
+import com.olzumzum.booklib.databinding.FragmentBookByDateListBinding
 import com.olzumzum.booklib.viewmodel.BookViewModel
 import javax.inject.Inject
 
@@ -36,29 +39,28 @@ class BookByDateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_book_by_date_list, container, false)
+
+        val binding: FragmentBookByDateListBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_book_by_date_list,
+            container,
+            false
+        )
+
+        val view = binding.root
+
+
+
 
         val adapter = BookRecyclerViewAdapter()
         val recyclerView = view.findViewById<RecyclerView>(R.id.books_by_date_lsit)
-        recyclerView.layoutManager = LinearLayoutManager(activity)
-        recyclerView.adapter = adapter
+        recyclerView?.layoutManager = LinearLayoutManager(activity)
+        recyclerView?.adapter = adapter
 
-        val bestsellersDate: TextView = view.findViewById(R.id.bestsellers_date)
 
-        val displayName: TextView = view.findViewById(R.id.display_name)
-
-        val nextPublishedDate: TextView = view.findViewById(R.id.next_published_date)
-        val publishedDateDescription: TextView = view.findViewById(R.id.published_date_description)
-        val published_date: TextView = view.findViewById(R.id.published_date)
-        val updated: TextView = view.findViewById(R.id.updated)
 
         viewModel.getResults().observe(viewLifecycleOwner, androidx.lifecycle.Observer { results ->
-            bestsellersDate.text = "${results.bestsellers_date}"
-            displayName.text = "${results.display_name}"
-            nextPublishedDate.text = "${results.next_published_date}"
-            published_date.text = "${results.published_date}"
-            publishedDateDescription.text = "${results.published_date_description}"
-            updated.text = "${results.updated}"
+            binding.results = results
             adapter.update(results.books)
         })
 
