1fd56f22e9fccf64a5ea69ad16c263bf5a02ddec
Olzumzum <evdokimova_olga_15@mail.ru>
2020-08-20 14:28:00 +0700

    change layout was opened to click

--- app/src/main/java/com/olzumzum/booklib/di/component/ActivitySubComponent.kt
+++ app/src/main/java/com/olzumzum/booklib/di/component/ActivitySubComponent.kt
@@ -3,6 +3,7 @@ package com.olzumzum.booklib.di.component
 import androidx.appcompat.app.AppCompatActivity
 import com.olzumzum.booklib.MainActivity
 import com.olzumzum.booklib.di.module.ActivityModule
+import com.olzumzum.booklib.ui.book_full_info.BookFullInfoFragment
 import com.olzumzum.booklib.ui.categories.CategoriesBestsellerFragment
 import com.olzumzum.booklib.ui.listbydata.BookByDateFragment
 import dagger.BindsInstance
@@ -21,4 +22,5 @@ interface ActivitySubComponent {
     fun inject(mainActivity: MainActivity)
     fun inject(categoriesBestsellerFragment: CategoriesBestsellerFragment)
     fun inject(bookByDateFragment: BookByDateFragment)
+    fun inject(bookFullInfoFragment: BookFullInfoFragment)
 }
--- app/src/main/java/com/olzumzum/booklib/ui/book_full_info/BookFullInfoFragment.kt
+++ app/src/main/java/com/olzumzum/booklib/ui/book_full_info/BookFullInfoFragment.kt
@@ -4,12 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.olzumzum.booklib.R
+import com.olzumzum.booklib.app.App
 import com.olzumzum.booklib.databinding.FragmentBookByDateListBinding
+import com.olzumzum.booklib.viewmodel.BookViewModel
+import javax.inject.Inject
 
 class BookFullInfoFragment: Fragment() {
+
+    @Inject
+    lateinit var viewModel: BookViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity?.application as App).getAppComponent().activitySubComponentBuilder()
+            .with(activity = activity as AppCompatActivity)
+            .build()
+            .inject(this)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,14 +34,14 @@ class BookFullInfoFragment: Fragment() {
     ): View? {
         val binding: FragmentBookByDateListBinding = DataBindingUtil.inflate(
             inflater,
-            R.layout.fragment_book_by_date_list,
+            R.layout.fragment_book_full_info,
             container,
             false
         )
         //биндинг данных во вью
         val view = binding.root
         binding.lifecycleOwner = this
-//        binding.viewModel = viewModel
+        binding.viewModel = viewModel
 
         return view
     }
